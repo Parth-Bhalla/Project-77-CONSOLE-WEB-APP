6d9f976449662eb27e50edc3022b1be8ff49bc2e
Parth-Bhalla <75519009+Parth-Bhalla@users.noreply.github.com>
2021-02-07 10:45:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+console.log("My first console project")
+
+var first_name="Parth";
+console.log(first_name);
+
+var last_name="Bhalla";
+console.log(last_name);
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name=full_name.length;
+console.log(length_of_full_name);
+
+var number1=60;
+var number2=30;
+var number3_of_multiplication=20;
+var number4_of_multiplication=20;
+
+var addition=number1 + number2;
+console.log(addition);
+
+var subtraction = number1 - number2;
+console.log(subtraction);
+
+var multiplication = number3_of_multiplication * number4_of_multiplication;
+console.log(multiplication);
+
+var division=number1 / number2;
+console.log(division);
